ae96fb51fedf4e0430a0b8faf4835f6a5a8ac5e1
Varkeychan Jacob <varkeychanjacob@Varkeychans-MacBook-Pro-2631.local>
2024-06-09 23:04:05 +0530

    testing framework

---
+++ test/Exporter/TestingExporter/TestingExporterTest.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace OpentelemetryDotnetCollector.Exporter.TestingExporter.Test;
+
+public class TestingExporterTest
+{
+    [Fact]
+    public void TestResourceAttributes()
+    {
+
+    }  
+}
